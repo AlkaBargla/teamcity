7fe0f5bcabb0ab1923e81e87a402643d284ff534
Alka Bargla <alkabargla@gmail.com>
2016-03-05 14:30:26 +0000

    First commit

---
+++ SourceCode/WebApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WebApplication1.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DisplayCustomer()
+        {
+            MyFirstController obj = new MyFirstController();
+            var result = obj.DisplayCustomer();
+            Assert.AreEqual("DisplayCustomer", result.ViewName);
+        }
+    }
+}
---
+++ SourceCode/WebApplication1/WebApplication1/Controllers/MyFirstController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MyFirstController : Controller
+    {
+        // GET: MyFirst
+        public ActionResult Index()
+        {
+            ViewData["CurrentTime"] = DateTime.Now.ToString();
+            return View("HelloView");
+        }
+
+        public ActionResult SayHello()
+        {
+            return View("HelloView");
+        }
+
+        public ViewResult DisplayCustomer()
+        {
+            Customer objcust = new Customer();
+            objcust.Id = 12;
+            objcust.CustomerCode = "1001";
+            objcust.Amount = 90.34;
+
+            return View("DisplayCustomer", objcust);
+        }
+        
+    }
+}
