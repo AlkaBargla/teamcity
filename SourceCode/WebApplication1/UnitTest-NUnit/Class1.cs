9a9ae138c6c99ff183d73e3ac0b24e272c6b55d3
Alka Bargla <alkabargla@gmail.com>
2016-03-05 22:06:03 +0000

    commit FxCop changes

---
+++ SourceCode/WebApplication1/UnitTest-NUnit/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebApplication1.Controllers;
+
+namespace UnitTest_NUnit
+{
+    [TestFixture]
+    public class Class1
+    {
+       [Test]
+        public void DisplayCustomer()
+        {
+            MyFirstController obj = new MyFirstController();
+            var result = obj.DisplayCustomer();
+            Assert.AreEqual("DisplayCustomer", result.ViewName);
+        }
+
+    }
+}
